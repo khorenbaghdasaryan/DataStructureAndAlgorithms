b33d01e62ac486e76154bc54490250272c10a1c2
Khoren Baghdasaryan <khoren.a.baghdasaryan@gmail.com>
2020-05-06 00:41:52 +0400

    1 Commit

---
+++ Collections/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collection
+{
+    class LinkedList
+    {
+        public void CreateLinkedList()
+        {
+            // Create the link list.
+            string[] words = { "the", "fox", "jumps", "over", "the", "dog" };
+            LinkedList<string> sentence = new LinkedList<string>(words);
+            Console.WriteLine($"sentence.Contains(\"jumps\") = {sentence.Contains("jumps")}");
+            Display(sentence, "The linked list values:");
+            
+
+            // Add the word 'today' to the beginning of the linked list.
+            sentence.AddFirst("today");
+            Display(sentence, "Test 1: Add 'today' to beginning of the list:");
+
+            // Move the first node to be the last node.
+            LinkedListNode<string> mark1 = sentence.First;
+            sentence.RemoveFirst();
+            sentence.AddLast(mark1);
+            Display(sentence, "Test 2: Move first node to be last node:");
+
+            // Change the last node to 'yesterday'.
+            sentence.RemoveFirst();
+            sentence.AddLast("yesterday");
+            Display(sentence, "Test 3: Change the last node to 'yesterday':");
+
+            // Move the last node to be the first node.
+            mark1 = sentence.Last;
+            sentence.RemoveLast();
+            sentence.AddFirst(mark1);
+            Display(sentence, "Test 4: Move last node to be first node:");
+
+            // Indicate the last occurence of 'the'.
+            sentence.RemoveFirst();
+            LinkedListNode<string> current = sentence.FindLast("the");
+            IndicateNode(current, "Test 5: Indicate last occurence of 'the':");
+        }
+
+        private void IndicateNode(LinkedListNode<string> current, string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Display(LinkedList<string> words, string test)
+        {
+            Console.WriteLine(test);
+            foreach (string word in words)
+            {
+                Console.Write(word + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new LinkedList().CreateLinkedList();
+        }
+    }
+}
