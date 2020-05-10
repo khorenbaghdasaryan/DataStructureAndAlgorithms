8fb5ff37c49263958ac8f9c7fd547b67ca20dc29
Khoren Baghdasaryan <khoren.a.baghdasaryan@gmail.com>
2020-05-10 17:22:23 +0400

    12 Commit

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Text;
 using System.Globalization;
 using System.IO;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
 
 namespace Collection
 {
@@ -534,11 +536,83 @@ namespace Collection
                 s.EndsWith(".docx"));
         }
     }
-
-    // Defines a comparer to create a sorted set
-    // that is sorted by the file extensions.
-    public class ByFileExtension : IComparer<string>
+    class ConcurrentBags
     {
+        public void Start()
+        {
+            ConcurrentBag<int> cd = new ConcurrentBag<int>();
+            List<Task> bagAddTasks = new List<Task>();
+
+            for (int i = 0; i < 500; i++)
+            {
+                var numberToAdd = i;
+                bagAddTasks.Add(Task.Run(() => cd.Add(numberToAdd)));
+            }
+
+            // Wait for all tasks to complete
+            Task.WaitAll(bagAddTasks.ToArray());
+
+            // Consume the items in the bag
+            List<Task> bagConsumeTasks = new List<Task>();
+            int itemsInBag = 0;
+            while (!cd.IsEmpty)
+            {
+                bagConsumeTasks.Add(Task.Run(() =>
+                {
+                    int item;
+                    if (cd.TryTake(out item))
+                    {
+                        Console.WriteLine(item);
+                        itemsInBag++;
+                    }
+                }));
+            }
+            Task.WaitAll(bagConsumeTasks.ToArray());
+
+            Console.WriteLine($"There were {itemsInBag} items in the bag");
+
+            // Checks the bag for an item
+            // The bag should be empty and this should not print anything
+            int unexpectedItem;
+            if (cd.TryPeek(out unexpectedItem))
+                Console.WriteLine("Found an item in the bag when it should be empty");
+        }
+    }
+    class ConcurrentDictionarys
+    {
+        // Demonstrates:
+        //      ConcurrentDictionary<TKey, TValue> ctor(concurrencyLevel, initialCapacity)
+        //      ConcurrentDictionary<TKey, TValue>[TKey]
+        public void Start()
+        {
+            // We know how many items we want to insert into the ConcurrentDictionary.
+            // So set the initial capacity to some prime number above that, to ensure that
+            // the ConcurrentDictionary does not need to be resized while initializing it.
+            int NUMITEMS = 64;
+            int initialCapacity = 101;
+
+            // The higher the concurrencyLevel, the higher the theoretical number of operations
+            // that could be performed concurrently on the ConcurrentDictionary.  However, global
+            // operations like resizing the dictionary take longer as the concurrencyLevel rises.
+            // For the purposes of this example, we'll compromise at numCores * 2.
+            int numProcs = Environment.ProcessorCount;
+            int concurrencyLevel = numProcs * 2;
+
+            // Construct the dictionary with the desired concurrencyLevel and initialCapacity
+            ConcurrentDictionary<int, int> cd = new ConcurrentDictionary<int, int>(concurrencyLevel, initialCapacity);
+
+            // Initialize the dictionary
+            for (int i = 0; i < NUMITEMS; i++) 
+                cd[i] = i * i;
+
+            Console.WriteLine($"The square of 23 is {cd[23]} (should be {23 * 23})");
+        }
+    }
+    
+    class ByFileExtension : IComparer<string>
+    {
+        // Defines a comparer to create a sorted set
+        // that is sorted by the file extensions.
         string xExt, yExt;
 
         CaseInsensitiveComparer caseiComp = new CaseInsensitiveComparer();
@@ -552,18 +626,13 @@ namespace Collection
             // Compare the file extensions.
             int vExt = caseiComp.Compare(xExt, yExt);
             if (vExt != 0)
-            {
                 return vExt;
-            }
             else
-            {
                 // The extension is the same,
                 // so compare the filenames.
                 return caseiComp.Compare(x, y);
-            }
         }
     }
-    
     class SortedLists
     {
         public void Start()
@@ -1085,7 +1154,7 @@ namespace Collection
         [Obsolete]
         static void Main(string[] args)
         {
-            new SortedSets().Start();
+            new ConcurrentDictionarys().Start();
         }
     }
 }
