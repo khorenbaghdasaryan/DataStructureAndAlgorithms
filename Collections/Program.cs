8788b4d1091d52e93b75b7398fab1745e919e74d
Khoren Baghdasaryan <khoren.a.baghdasaryan@gmail.com>
2020-05-12 00:27:10 +0400

    17 Commit

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -1403,24 +1403,171 @@ namespace Collection
             Console.WriteLine("Displays the elements using foreach:");
             PrintKeysAndValues1(myCol);
 
+            // Display the contents of the collection using the enumerator.
+            Console.WriteLine("Displays the elements using the IDictionaryEnumerator:");
+            PrintKeysAndValues2(myCol);
+
+            // Display the contents of the collection using the Keys, Values, Count, and Item properties.
+            Console.WriteLine("Displays the elements using the Keys, Values, Count, and Item properties:");
+            PrintKeysAndValues3(myCol);
+
+            // Copies the HybridDictionary to an array with DictionaryEntry elements.
+            DictionaryEntry[] myArr = new DictionaryEntry[myCol.Count];
+            myCol.CopyTo(myArr, 0);
+
+            // Displays the values in the array.
+            Console.WriteLine("Displays the elements in the array:");
+            Console.WriteLine("   KEY                       VALUE");
+            for (int i = 0; i < myArr.Length; i++)
+                Console.WriteLine($"   {myArr[i].Key,-25} {myArr[i].Value}");
+            Console.WriteLine();
+
+            // Searches for a key.
+            if (myCol.Contains("Kiwis"))
+                Console.WriteLine("The collection contains the key \"Kiwis\".");
+            else
+                Console.WriteLine("The collection does not contain the key \"Kiwis\".");
+            Console.WriteLine();
+
+            // Deletes a key.
+            myCol.Remove("Plums");
+            Console.WriteLine("The collection contains the following elements after removing \"Plums\":");
+            PrintKeysAndValues1(myCol);
+
+            // Clears the entire collection.
+            myCol.Clear();
+            Console.WriteLine("The collection contains the following elements after it is cleared:");
+            PrintKeysAndValues1(myCol);
         }
 
-        private void PrintKeysAndValues1(IDictionary myCol)
+        // Uses the foreach statement which hides the complexity of the enumerator.
+        // NOTE: The foreach statement is the preferred way of enumerating the contents of a collection.
+        public static void PrintKeysAndValues1(IDictionary myCol)
         {
             Console.WriteLine("   KEY                       VALUE");
             foreach (DictionaryEntry de in myCol)
-                Console.WriteLine($"  {de.Key,-25} {de.Value}");
+                Console.WriteLine($"   {de.Key,-25} {de.Value}" );
+            Console.WriteLine();
+        }
+
+        // Uses the enumerator.
+        // NOTE: The foreach statement is the preferred way of enumerating the contents of a collection.
+        public static void PrintKeysAndValues2(IDictionary myCol)
+        {
+            IDictionaryEnumerator myEnumerator = myCol.GetEnumerator();
+            Console.WriteLine("   KEY                       VALUE");
+            while (myEnumerator.MoveNext())
+                Console.WriteLine($"   {myEnumerator.Key,-25} {myEnumerator.Value}");
+            Console.WriteLine();
+        }
+
+        // Uses the Keys, Values, Count, and Item properties.
+        public static void PrintKeysAndValues3(HybridDictionary myCol)
+        {
+            String[] myKeys = new String[myCol.Count];
+            myCol.Keys.CopyTo(myKeys, 0);
+
+            Console.WriteLine("   INDEX KEY                       VALUE");
+            for (int i = 0; i < myCol.Count; i++)
+                Console.WriteLine($"   {i,-5} {myKeys[i],-25} {myCol[myKeys[i]]}");
             Console.WriteLine();
         }
     }
+    class ListDictionarys
+    {
+        public void Start()
+        {
+            // Creates and initializes a new ListDictionary.
+            ListDictionary myCol = new ListDictionary();
+            myCol.Add("Braeburn Apples", "1.49");
+            myCol.Add("Fuji Apples", "1.29");
+            myCol.Add("Gala Apples", "1.49");
+            myCol.Add("Golden Delicious Apples", "1.29");
+            myCol.Add("Granny Smith Apples", "0.89");
+            myCol.Add("Red Delicious Apples", "0.99");
+            myCol.Add("Plantain Bananas", "1.49");
 
+            // Display the contents of the collection using foreach. This is the preferred method.
+            Console.WriteLine("Displays the elements using foreach:");
+            PrintKeysAndValues1(myCol);
+
+            // Display the contents of the collection using the enumerator.
+            Console.WriteLine("Displays the elements using the IDictionaryEnumerator:");
+            PrintKeysAndValues2(myCol);
+
+            // Display the contents of the collection using the Keys, Values, Count, and Item properties.
+            Console.WriteLine("Displays the elements using the Keys, Values, Count, and Item properties:");
+            PrintKeysAndValues3(myCol);
+
+            // Copies the ListDictionary to an array with DictionaryEntry elements.
+            DictionaryEntry[] myArr = new DictionaryEntry[myCol.Count];
+            myCol.CopyTo(myArr, 0);
+
+            // Displays the values in the array.
+            Console.WriteLine("Displays the elements in the array:");
+            Console.WriteLine("   KEY                       VALUE");
+            for (int i = 0; i < myArr.Length; i++)
+                Console.WriteLine($"   {myArr[i].Key,-25} {myArr[i].Value}" );
+            Console.WriteLine();
+
+            // Searches for a key.
+            if (myCol.Contains("Kiwis"))
+                Console.WriteLine("The collection contains the key \"Kiwis\".");
+            else
+                Console.WriteLine("The collection does not contain the key \"Kiwis\".");
+            Console.WriteLine();
+
+            // Deletes a key.
+            myCol.Remove("Plums");
+            Console.WriteLine("The collection contains the following elements after removing \"Plums\":");
+            PrintKeysAndValues1(myCol);
+
+            // Clears the entire collection.
+            myCol.Clear();
+            Console.WriteLine("The collection contains the following elements after it is cleared:");
+            PrintKeysAndValues1(myCol);
+        }
+
+        // Uses the foreach statement which hides the complexity of the enumerator.
+        // NOTE: The foreach statement is the preferred way of enumerating the contents of a collection.
+        public void PrintKeysAndValues1(IDictionary myCol)
+        {
+            Console.WriteLine("   KEY                       VALUE");
+            foreach (DictionaryEntry de in myCol)
+                Console.WriteLine($"   {de.Key,-25} {de.Value}");
+            Console.WriteLine();
+        }
+
+        // Uses the enumerator.
+        // NOTE: The foreach statement is the preferred way of enumerating the contents of a collection.
+        public void PrintKeysAndValues2(IDictionary myCol)
+        {
+            IDictionaryEnumerator myEnumerator = myCol.GetEnumerator();
+            Console.WriteLine("   KEY                       VALUE");
+            while (myEnumerator.MoveNext())
+                Console.WriteLine($"   {myEnumerator.Key,-25} {myEnumerator.Value}");
+            Console.WriteLine();
+        }
+
+        // Uses the Keys, Values, Count, and Item properties.
+        public static void PrintKeysAndValues3(ListDictionary myCol)
+        {
+            String[] myKeys = new String[myCol.Count];
+            myCol.Keys.CopyTo(myKeys, 0);
+
+            Console.WriteLine("   INDEX KEY                       VALUE");
+            for (int i = 0; i < myCol.Count; i++)
+                Console.WriteLine($"   {i,-5} {myKeys[i],-25} {myCol[myKeys[i]]}");
+            Console.WriteLine();
+        }
+    }
 
     class Program
     {
         [Obsolete]
         static void Main(string[] args)
         {
-            new HybridDictionarys().Start();
+            new ListDictionarys().Start();
         }
     }
 }
