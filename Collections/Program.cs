2af8b8f3703590ec10cee86484f01fe14ca27baa
Khoren Baghdasaryan <khoren.a.baghdasaryan@gmail.com>
2020-05-12 16:05:19 +0400

    19 Commit

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -1564,7 +1564,6 @@ namespace Collection
             Console.WriteLine();
         }
     }
-
     class NameValueCollections
     {
         // Represents a collection of associated String keys and String values that 
@@ -1622,12 +1621,185 @@ namespace Collection
             Console.WriteLine();
         }
     }
+    class OrderedDictionarys
+    {
+        public void Start()
+        {
+            // Creates and initializes a OrderedDictionary.
+            OrderedDictionary myOrderedDictionary = new OrderedDictionary();
+            myOrderedDictionary.Add("testKey1", "testValue1");
+            myOrderedDictionary.Add("testKey2", "testValue2");
+            myOrderedDictionary.Add("keyToDelete", "valueToDelete");
+            myOrderedDictionary.Add("testKey3", "testValue3");
+
+            ICollection keyCollection = myOrderedDictionary.Keys;
+            ICollection valueCollection = myOrderedDictionary.Values;
+
+            // Display the contents using the key and value collections
+            DisplayContents(keyCollection, valueCollection, myOrderedDictionary.Count);
+
+            // Modifying the OrderedDictionary
+            if (!myOrderedDictionary.IsReadOnly)
+            {
+                // Insert a new key to the beginning of the OrderedDictionary
+                myOrderedDictionary.Insert(0, "insertedKey1", "insertedValue1");
+
+                // Modify the value of the entry with the key "testKey2"
+                myOrderedDictionary["testKey2"] = "modifiedValue";
+
+                // Remove the last entry from the OrderedDictionary: "testKey3"
+                myOrderedDictionary.RemoveAt(myOrderedDictionary.Count - 1);
+
+                // Remove the "keyToDelete" entry, if it exists
+                if (myOrderedDictionary.Contains("keyToDelete"))
+                    myOrderedDictionary.Remove("keyToDelete");
+            }
+
+            Console.WriteLine($"{Environment.NewLine} Displaying the entries of a modified OrderedDictionary.");
+            DisplayContents(keyCollection, valueCollection, myOrderedDictionary.Count);
+
+            // Clear the OrderedDictionary and add new values
+            myOrderedDictionary.Clear();
+            myOrderedDictionary.Add("newKey1", "newValue1");
+            myOrderedDictionary.Add("newKey2", "newValue2");
+            myOrderedDictionary.Add("newKey3", "newValue3");
+
+            // Display the contents of the "new" Dictionary using an enumerator
+            IDictionaryEnumerator myEnumerator = myOrderedDictionary.GetEnumerator();
+            Console.WriteLine($"{Environment.NewLine} Displaying the entries of a \"new\" OrderedDictionary.");
+            DisplayEnumerator(myEnumerator);
+
+            Console.ReadLine();
+        }
+
+        // Displays the contents of the OrderedDictionary from its keys and values
+        public void DisplayContents(
+            ICollection keyCollection, ICollection valueCollection, int dictionarySize)
+        {
+            String[] myKeys = new String[dictionarySize];
+            String[] myValues = new String[dictionarySize];
+            keyCollection.CopyTo(myKeys, 0);
+            valueCollection.CopyTo(myValues, 0);
+
+            // Displays the contents of the OrderedDictionary
+            Console.WriteLine("   INDEX KEY                       VALUE");
+            for (int i = 0; i < dictionarySize; i++)
+            {
+                Console.WriteLine($"   {i,-5} {myKeys[i],-25} {myValues[i]}");
+            }
+            Console.WriteLine();
+        }
+
+        // Displays the contents of the OrderedDictionary using its enumerator
+        public void DisplayEnumerator(IDictionaryEnumerator myEnumerator)
+        {
+            Console.WriteLine("   KEY                       VALUE");
+            while (myEnumerator.MoveNext())
+                Console.WriteLine($"   {myEnumerator.Key,-25} {myEnumerator.Value}" );
+        }
+    }
+
+    class StringCollections
+    {
+        public void Start()
+        {
+            // Create and initializes a new StringCollection.
+            StringCollection myCol = new StringCollection();
+
+            // Add a range of elements from an array to the end of the StringCollection.
+            String[] myArr = new String[] { "RED", "orange", "yellow", "RED", "green",
+                "blue", "RED", "indigo", "violet", "RED" };
+            myCol.AddRange(myArr);
+
+            // Display the contents of the collection using foreach. This is the preferred method.
+            Console.WriteLine("Displays the elements using foreach:");
+            PrintValues1(myCol);
+
+            // Display the contents of the collection using the enumerator.
+            Console.WriteLine("Displays the elements using the IEnumerator:");
+            PrintValues2(myCol);
+
+            // Display the contents of the collection using the Count and Item properties.
+            Console.WriteLine("Displays the elements using the Count and Item properties:");
+            PrintValues3(myCol);
+
+            // Add one element to the end of the StringCollection and insert another at index 3.
+            myCol.Add("* white");
+            myCol.Insert(3, "* gray");
+
+            Console.WriteLine("After adding \"* white\" to the end and inserting \"* gray\" at index 3:");
+            PrintValues1(myCol);
+
+            // Remove one element from the StringCollection.
+            myCol.Remove("yellow");
+
+            Console.WriteLine("After removing \"yellow\":");
+            PrintValues1(myCol);
+
+            // Remove all occurrences of a value from the StringCollection.
+            int i = myCol.IndexOf("RED");
+            while (i > -1)
+            {
+                myCol.RemoveAt(i);
+                i = myCol.IndexOf("RED");
+            }
+
+            // Verify that all occurrences of "RED" are gone.
+            if (myCol.Contains("RED"))
+                Console.WriteLine("*** The collection still contains \"RED\".");
+
+            Console.WriteLine("After removing all occurrences of \"RED\":");
+            PrintValues1(myCol);
+
+            // Copy the collection to a new array starting at index 0.
+            String[] myArr2 = new String[myCol.Count];
+            myCol.CopyTo(myArr2, 0);
+
+            Console.WriteLine("The new array contains:");
+            for (i = 0; i < myArr2.Length; i++)
+                Console.WriteLine($"   [{i}] {myArr2[i]}" );
+            Console.WriteLine();
+
+            // Clears the entire collection.
+            myCol.Clear();
+
+            Console.WriteLine("After clearing the collection:");
+            PrintValues1(myCol);
+        }
+
+        // Uses the foreach statement which hides the complexity of the enumerator.
+        // NOTE: The foreach statement is the preferred way of enumerating the contents of a collection.
+        public void PrintValues1(StringCollection myCol)
+        {
+            foreach (Object obj in myCol)
+                Console.WriteLine($"   {obj}" );
+            Console.WriteLine();
+        }
+
+        // Uses the enumerator.
+        // NOTE: The foreach statement is the preferred way of enumerating the contents of a collection.
+        public void PrintValues2(StringCollection myCol)
+        {
+            StringEnumerator myEnumerator = myCol.GetEnumerator();
+            while (myEnumerator.MoveNext())
+                Console.WriteLine($"   {myEnumerator.Current}" );
+            Console.WriteLine();
+        }
+
+        // Uses the Count and Item properties.
+        public void PrintValues3(StringCollection myCol)
+        {
+            for (int i = 0; i < myCol.Count; i++)
+                Console.WriteLine($"   {myCol[i]}" );
+            Console.WriteLine();
+        }
+    }
     class Program
     {
         [Obsolete]
         static void Main(string[] args)
         {
-            new NameValueCollections().Start();
+            new StringCollections().Start();
         }
     }
 }
